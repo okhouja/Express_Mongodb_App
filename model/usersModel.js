83c29ca97075ec41d226c298e49a3091926b8bbe
Omar <omar0940@gmail.com>
2021-08-26 15:53:46 +0200

    DB Schema

--- model/usersModel.js
+++ model/usersModel.js
@@ -13,9 +13,31 @@ const userDataSchema = new mongoose.Schema({
   age: {
     type: Integer,
     trim: true,
-    require: [true, "You need to enter your age"],
+    require: [true, "You need to enter your age as Interger"],
+  },
+  fbw: {
+    type: Integer,
+    trim: true,
+    require: [true, "You need to enter your Class Nubmer"],
+  },
+  toolStack: {
+    type: String,
+    trim: true,
+    require: [true, "You need to enter"],
+  },
+  email: {
+    type: String,
+    trim: true,
+    require: [true, "Please, Enter your Email Address"],
+  },
+  userAddedDate: {
+    type: Date,
+    require: true,
+    default: Date.now,
   },
 });
+
+module.exports = mongoose.model("UserData", userDataSchema);
 /*{
     "userName": "steel",
     "userPass": "123pass",
