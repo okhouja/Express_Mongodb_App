523c82cbb31714c6fab5b2133558a1a814b0d1e0
Omar <omar0940@gmail.com>
2021-08-26 16:28:56 +0200

    DB contriller & router

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,18 @@
 const UserData = require("../model/usersModel");
 const express = require("express");
 
+// View All Users
+const getAllUsers = async (req, res) => {
+  try {
+    const user = await UserData.find();
+    res.status(200).json();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 // Add new user
-const addUser = async (req, res) => {
+const addNewUser = async (req, res) => {
   const user = new UserData({
     userName: req.body.userName,
     userPass: req.body.userPass,
@@ -11,6 +21,12 @@ const addUser = async (req, res) => {
     toolStack: req.body.toolStack,
     email: req.body.email,
   });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 /*{
--- router/users.js
+++ router/users.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const router = require("router");
 const UserData = require("../model/usersModel");
+const { getAllUsers, addNewUser } = require("../controllers/usersControllers");
+
+router.route("/").get(getAllUsers).post(addNewUser);
+
+module.exports = router;
